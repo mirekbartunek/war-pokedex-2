8060e314dcf2ea73ed90f4958401094f75bf6e8c
mirekbartunek <mucus.hooide@gmail.com>
2023-04-17 09:18:57 +0200

    analytics

--- src/components/be/Fetcher.ts
+++ src/components/be/Fetcher.ts
@@ -1,11 +1,4 @@
 export const fetcher = async (endpoint: string) => {
   const response = await fetch(endpoint);
-  if (!response.ok) {
-    switch (response.status) {
-      case 404:
-        throw new Error("Not found");
-    }
-  }
-
   return await response.json();
 };
